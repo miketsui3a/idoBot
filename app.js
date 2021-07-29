ac3f97d99324f88962835c38ec76d45e65a8ed98
miketsui3a <42023809+miketsui3a@users.noreply.github.com>
2021-07-29 18:53:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const Web3 = require("web3");
 // const abiDecoder = require("abi-decoder");
 
 // let web3 = new Web3("https://rpc-mainnet.matic.network");
-let web3 = new Web3("http://127.0.0.1:8545");
+let web3 = new Web3("https://bsc-dataseed.binance.org/");
 
 const factoryAbi = [
   {
@@ -229,19 +229,247 @@ const swapAbi = [
 ];
 const pairAbi = [];
 
-const swapAddr = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
-const factoryAddr = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
+const erc20Abi = [
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+          {
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_spender",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "approve",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_from",
+              "type": "address"
+          },
+          {
+              "name": "_to",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint8"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [
+          {
+              "name": "_owner",
+              "type": "address"
+          }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+          {
+              "name": "balance",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+          {
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_to",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "transfer",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [
+          {
+              "name": "_owner",
+              "type": "address"
+          },
+          {
+              "name": "_spender",
+              "type": "address"
+          }
+      ],
+      "name": "allowance",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "fallback"
+  },
+  {
+      "anonymous": false,
+      "inputs": [
+          {
+              "indexed": true,
+              "name": "owner",
+              "type": "address"
+          },
+          {
+              "indexed": true,
+              "name": "spender",
+              "type": "address"
+          },
+          {
+              "indexed": false,
+              "name": "value",
+              "type": "uint256"
+          }
+      ],
+      "name": "Approval",
+      "type": "event"
+  },
+  {
+      "anonymous": false,
+      "inputs": [
+          {
+              "indexed": true,
+              "name": "from",
+              "type": "address"
+          },
+          {
+              "indexed": true,
+              "name": "to",
+              "type": "address"
+          },
+          {
+              "indexed": false,
+              "name": "value",
+              "type": "uint256"
+          }
+      ],
+      "name": "Transfer",
+      "type": "event"
+  }
+]
 
-const inTokenAddr = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
-const outTokenAddr = "0xc2132d05d31c914a87c6611c10748aeb04b58e8f";
+const swapAddr = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+const factoryAddr = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
+
+const inTokenAddr = "0xe9e7cea3dedca5984780bafc599bd69add087d56";
+const outTokenAddr = "0x3c1748d647e6a56b37b66fcd2b5626d0461d3aa0";
 
 const myAddr = "0xd8aBbc1AAC264f84d3aae1e6E501fe8EC3EB99d1";
 
 const yourPrivateKey = "";
+const BN = web3.utils.BN;
 
 async function main() {
   const factory = new web3.eth.Contract(factoryAbi, factoryAddr);
   const swap = new web3.eth.Contract(swapAbi, swapAddr);
+
+  const tokenInContract = new web3.eth.Contract(erc20Abi, inTokenAddr)
+  const tokenOutContract = new web3.eth.Contract(erc20Abi, outTokenAddr)
+
 
   while (true) {
     const pairAddr = await factory.methods
@@ -249,34 +477,48 @@ async function main() {
       .call();
     console.log(pairAddr);
     if (pairAddr != "0x0000000000000000000000000000000000000000") {
-      const swapTx = await swap.methods.swapExactTokensForTokens(
-        10000,
-        0,
-        [
-          inTokenAddr,
-          outTokenAddr,
-        ],
-        myAddr,
-        // currentBlock + 10
-        99999999999
-      );
+      console.log("bingo pair")
 
-      const swapSigned = await web3.eth.accounts.signTransaction(
-        {
-          to: swapAddr,
-          data: swapTx.encodeABI(),
-          gas: "5000000",
-          gasPrice: "50000000000",
-        },
-        yourPrivateKey
-      );
+      while(true){
+        
+        const balanceInPool = new BN(await tokenInContract.methods.balanceOf(pairAddr).call())
+        console.log(new Date(), balanceInPool.toString())
+        const isNotZero = balanceInPool.gt(new BN('1234'))
+        if(isNotZero){
+          console.log("non zero")
+          const swapTx = await swap.methods.swapExactTokensForTokens(
+            "100000000000000000000",
+            0,
+            [
+              inTokenAddr,
+              outTokenAddr,
+            ],
+            myAddr,
+            // currentBlock + 10
+            99999999999
+          );
+    
+          const swapSigned = await web3.eth.accounts.signTransaction(
+            {
+              to: swapAddr,
+              data: swapTx.encodeABI(),
+              gas: "5000000",
+              gasPrice: "5000000001",
+            },
+            yourPrivateKey
+          );
+    
+          const swapResponse = await web3.eth.sendSignedTransaction(
+            swapSigned.rawTransaction
+          );
+    
+          console.log(swapResponse)
+          return
+  
+        }
+      }
+      
 
-      const swapResponse = await web3.eth.sendSignedTransaction(
-        swapSigned.rawTransaction
-      );
-
-      console.log(swapResponse)
-      break
 
     }
   }
